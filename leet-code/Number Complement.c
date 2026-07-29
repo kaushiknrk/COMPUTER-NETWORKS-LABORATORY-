76dd3dc98d3ac9873a91b8551821564aa8085d7f
KAUSHIK NR <kaushiknr326@gmail.com>
2026-07-29 15:53:34 +0530

    Number Complement

--- leet-code/Number Complement.c
+++ leet-code/Number Complement.c
@@ -1,1 +1,13 @@
+int findComplement(int num) {
 
+    int complement=0;
+    unsigned int value=1;
+    while(num>0){
+        int bin=num%2;
+        int change=1-bin;
+        complement = complement +(change * value);
+        value = value * 2;
+        num=num/2;
+    }
+    return complement;
+}
